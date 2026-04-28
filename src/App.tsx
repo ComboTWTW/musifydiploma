170de0d518c434362d8632ea57464761e38f4815
ComboTWTW <gfakemail18@gmail.com>
2026-04-29 00:38:29 +0500

    Add: Router, Tailwind set up,

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+
 const App = () => {
-    return <div className="w-full h-screen overflow-hidden flex "></div>;
+    return (
+        <div className="w-full h-screen overflow-hidden flex flex-col items-center xl:bg-bgMain">
+            <Router>
+                <div className="max-w-300 w-full">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+
+                        <Route path="*" element={<PageNotFound />} />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
