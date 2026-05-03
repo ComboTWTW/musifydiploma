ec0a99c3ee4cdce62cc0041c8931abce0a6bd506
ComboTWTW <gfakemail18@gmail.com>
2026-05-04 03:49:08 +0500

    add: basic pages routes and simple navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,68 @@
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import {
+    Routes,
+    Route,
+    BrowserRouter as Router,
+    Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 import Navbar from "./components/Navbar";
+import SignUpPage from "./pages/SignUpPage";
+import Profile from "./pages/Profile";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
+import { useState } from "react";
+import SearchBar from "./components/Navbar/SearchBar";
+import SearchPage from "./pages/SearchPage";
+import Artist from "./pages/Artist";
 
 const App = () => {
+    const [isSigned, setIsSigned] = useState<boolean | null>(null);
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setIsSigned(() => true);
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            const uid = user.uid;
+            // ...
+        } else {
+            // User is signed out
+            // ...
+            setIsSigned(() => false);
+        }
+    });
     return (
         <div className="w-full h-screen overflow-hidden flex flex-col items-center bg-bgMain">
-            <Router>
-                <Navbar />
-                <div className="max-w-360 w-full px-4">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
+            {isSigned !== null && (
+                <Router>
+                    <Navbar />
+                    <div className="max-w-360 w-full px-4 flex items-center">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/signup" element={<SignUpPage />} />
+                            <Route path="/search?" element={<SearchPage />} />
 
-                        <Route path="*" element={<PageNotFound />} />
-                    </Routes>
-                </div>
-            </Router>
+                            <Route path="/artist?" element={<Artist />} />
+
+                            <Route
+                                path="/profile"
+                                element={
+                                    isSigned === true ? (
+                                        <Profile />
+                                    ) : (
+                                        isSigned === false && (
+                                            <Navigate to="/" />
+                                        )
+                                    )
+                                }
+                            />
+
+                            <Route path="*" element={<PageNotFound />} />
+                        </Routes>
+                    </div>
+                </Router>
+            )}
         </div>
     );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,14 @@ import { NavLink } from "react-router-dom";
 import { logoMain } from "../assets/assets";
 import { navbarButtons, navLinks } from "../constants/constValues";
 import SearchBar from "./Navbar/SearchBar";
+import { auth } from "../config/firebase";
 
 const Navbar = () => {
+    const user = auth.currentUser;
+
     return (
-        <nav className="w-full bg-border flex flex-col items-center px-4 py-2">
-            <div className="max-w-360 w-full flex items-center justify-evenly">
+        <nav className="w-full bg-border flex flex-col items-center justify-center  py-2">
+            <div className="max-w-360 w-full flex items-center justify-between px-4">
                 {/* Logo */}
                 <NavLink to={"/"} reloadDocument>
                     <img src={logoMain} alt="Logo" className="max-w-42" />
@@ -30,19 +33,29 @@ const Navbar = () => {
                 {/* SearchBar Component and SignIn Buttons*/}
                 <div className="flex gap-9 items-center">
                     <SearchBar />
-                    <ul className="flex gap-3">
-                        {navbarButtons.map((button, index) => {
-                            return (
-                                <NavLink to={button.path} reloadDocument>
-                                    <li
-                                        className={`border-purpleMain border border-solid ${button.id === "signup" ? "bg-purpleMain" : "bg-border"} rounded-[10px] text-whiteMain font-inter font-light py-2.5 px-9 `}
-                                    >
-                                        {button.title}
-                                    </li>
-                                </NavLink>
-                            );
-                        })}
-                    </ul>
+                    {user === null ? (
+                        <ul className="flex gap-3">
+                            {navbarButtons.map((button, index) => {
+                                return (
+                                    <NavLink to={button.path} reloadDocument>
+                                        <li
+                                            className={`border-purpleMain border border-solid ${button.id === "signup" ? "bg-purpleMain" : "bg-border"} rounded-[10px] text-whiteMain font-inter font-light py-2.5 px-9 `}
+                                        >
+                                            {button.title}
+                                        </li>
+                                    </NavLink>
+                                );
+                            })}
+                        </ul>
+                    ) : (
+                        <NavLink to={"/profile"} reloadDocument>
+                            <img
+                                src={`${user.photoURL}`}
+                                alt="Profile Picture"
+                                className="rounded-full max-h-15"
+                            />
+                        </NavLink>
+                    )}
                 </div>
             </div>
         </nav>
